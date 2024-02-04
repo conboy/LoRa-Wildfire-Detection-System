970e13f705240277a342a76f434adddc1221893f
conboy <conjamalex@gmail.com>
2024-02-04 10:19:54 -0500

    Map working

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,20 @@
-import Navbar from "./components/Navbar";
-import MapComponent from "./components/MapComponent";
+"use client"
+
+import Navbar from "@/components/Navbar";
+import dynamic from 'next/dynamic'
+
 
 export default function Home() {
+  const Map = dynamic(
+    () => import('@/components/Map'), // replace '@components/map' with your component's location
+    { ssr: false } // This line is important. It's what prevents server-side render
+  )
+
   return (
     <main>
       
       <Navbar />
-      <MapComponent />
+      <Map />
     </main>
   );
 }
