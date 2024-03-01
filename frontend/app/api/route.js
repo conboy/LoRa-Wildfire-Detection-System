ff8c2af7e1b3fd73b6ff4dbf11f1f47e0b52daa0
conboy <conjamalex@gmail.com>
2024-03-01 01:49:02 -0500

    Added upsert nodes functionality to API

--- frontend/app/api/route.js
+++ frontend/app/api/route.js
@@ -1,9 +1,24 @@
+import { createClient } from '@supabase/supabase-js';
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 export async function GET() {
     return new Response('Hello, Next.js!', { status: 200 })
 }
 
 export async function POST(request) {
     const data = await request.json();
-    return new Response(JSON.stringify(data), { status: 200 });
+    console.log(data.nodes);
+    const { response, error } = await supabase
+        .from('nodes')
+        .upsert(data.nodes)
+
+    if (error) {
+        console.error('Error upserting data:', error);
+        return new Response(JSON.stringify({ error: error.message }), { status: 500 });
+    } else {
+        return new Response(JSON.stringify({ message: "Upsert successful" }), { status: 200 });
+    }
 }
 
