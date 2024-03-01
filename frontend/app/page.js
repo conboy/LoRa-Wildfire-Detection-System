c2cbe9f4cceb1f7415e2fe17dc50a58ffd56ccee
conboy <conjamalex@gmail.com>
2024-02-29 22:45:54 -0500

    Remove comments from page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -6,8 +6,8 @@ import dynamic from 'next/dynamic'
 
 export default function Home() {
   const Map = dynamic(
-    () => import('@/components/Map'), // replace '@components/map' with your component's location
-    { ssr: false } // This line is important. It's what prevents server-side render
+    () => import('@/components/Map'),
+    { ssr: false }
   )
 
   return (
